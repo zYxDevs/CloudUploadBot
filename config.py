a98ffaf4dc36facd9a2851a629e18a530d562355
Yoga Pranata <ahmadzaylani001@gmail.com>
2023-04-18 19:17:32 +0700

    config files

---
+++ config.py
@@ -0,0 +1,5 @@
+from os import getenv
+
+API_ID = getenv("API_ID", 0)  # from my.telegram.org
+API_HASH = getenv("API_HASH", "Any")  # from my.telegram.org
+BOT_TOKEN = getenv("BOT_TOKEN", "Any")  # get from t.me/botfather
